d221b46ee3b591dd19978be30f819ce374bbc14d
Ivan <ivan.evseychik@gmail.com>
2023-03-12 01:11:57 +0300

    Adapt Vector class for range-based loops.

--- src/vector_iterator.hpp
+++ src/vector_iterator.hpp
@@ -10,7 +10,7 @@ namespace stl_container_impl
     class pointer_wrapper_iterator
     {
     protected:
-        Iterator m_iterator;
+        Iterator m_ptr;
 
         using traits_type = std::iterator_traits<Iterator>;
 
@@ -26,43 +26,46 @@ namespace stl_container_impl
         using pointer = typename traits_type::pointer;
 
         pointer_wrapper_iterator() noexcept
-            : m_iterator(Iterator()) { }
+            : m_ptr(Iterator()) { }
 
         explicit pointer_wrapper_iterator(const Iterator& i) noexcept
-            : m_iterator(i) { }
+            : m_ptr(i) { }
 
         template<typename Iter, typename = convertible_from<Iter>>
         pointer_wrapper_iterator(const pointer_wrapper_iterator<Iter, Container>& i) noexcept
-            : m_iterator(i.base()) { }
+            : m_ptr(i.base()) { }
 
         // Forward iterator requirements
-        reference operator*() const noexcept { return *m_iterator; }
-        pointer operator->() const noexcept { return m_iterator; }
+        reference operator*() const noexcept { return *m_ptr; }
+        pointer operator->() const noexcept { return m_ptr; }
 
         pointer_wrapper_iterator& operator++()noexcept
         {
-            ++m_iterator;
+            ++m_ptr;
             return *this;
         }
 
-        pointer_wrapper_iterator operator++(int)noexcept { return pointer_wrapper_iterator(m_iterator++); }
+        pointer_wrapper_iterator operator++(int)noexcept { return pointer_wrapper_iterator(m_ptr++); }
+
+		bool operator!= (const pointer_wrapper_iterator& other) const noexcept { return m_ptr != other.m_ptr; }
+		bool operator== (const pointer_wrapper_iterator& other) const noexcept { return m_ptr == other.m_ptr; }
 
         // Bidirectional iterator requirements
         pointer_wrapper_iterator& operator--() noexcept
         {
-            --m_iterator;
+            --m_ptr;
             return *this;
         }
 
-        pointer_wrapper_iterator operator--(int) noexcept { return pointer_wrapper_iterator(m_iterator--); }
+        pointer_wrapper_iterator operator--(int) noexcept { return pointer_wrapper_iterator(m_ptr--); }
 
         // Random access iterator requirements
-        reference operator[](difference_type n) const noexcept { return m_iterator[n]; }
-        pointer_wrapper_iterator& operator+=(difference_type n) noexcept { m_iterator += n; return *this; }
-        pointer_wrapper_iterator operator+(difference_type n) const noexcept { return pointer_wrapper_iterator(m_iterator + n); }
-        pointer_wrapper_iterator& operator-=(difference_type n) noexcept { m_iterator -= n; return *this; }
-        pointer_wrapper_iterator operator-(difference_type n) const noexcept { return pointer_wrapper_iterator(m_iterator - n); }
-        const Iterator& base() const noexcept { return m_iterator; }
+        reference operator[](difference_type n) const noexcept { return m_ptr[n]; }
+        pointer_wrapper_iterator& operator+=(difference_type n) noexcept { m_ptr += n; return *this; }
+        pointer_wrapper_iterator operator+(difference_type n) const noexcept { return pointer_wrapper_iterator(m_ptr + n); }
+        pointer_wrapper_iterator& operator-=(difference_type n) noexcept { m_ptr -= n; return *this; }
+        pointer_wrapper_iterator operator-(difference_type n) const noexcept { return pointer_wrapper_iterator(m_ptr - n); }
+        const Iterator& base() const noexcept { return m_ptr; }
     };
 
 } // namespace stl_container_impl
